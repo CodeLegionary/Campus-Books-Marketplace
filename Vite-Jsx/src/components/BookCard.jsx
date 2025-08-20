45cf3aa1b1596bb6591180d10a34a70aba298d42
Gian Domenico <giando64f95@gmail.com>
2025-08-20 20:00:51 +0200

    polishing

--- Vite-Jsx/src/components/BookCard.jsx
+++ Vite-Jsx/src/components/BookCard.jsx
@@ -10,38 +10,16 @@ const BookCard = ({ book, onSimpleReport, onBuyBook }) => { // Only accept neces
       {book.ownerId && ( // Only show buttons if there's an owner
         <p>
           {onSimpleReport && ( // Only show report button if the function is provided
-            <button
+            <button className="report-button"
               onClick={() => onSimpleReport(book.ownerId, book.id)}
-              style={{
-                backgroundColor: '#ffc107',
-                color: 'black',
-                border: 'none',
-                padding: '5px 10px',
-                borderRadius: '3px',
-                cursor: 'pointer',
-                marginRight: '10px',
-                fontSize: '0.8em',
-                transition: 'background-color 0.2s ease',
-              }}
             >
               Segnala Utente
             </button>
           )}
 
           {onBuyBook && ( // Only show buy button if the function is provided
-            <button
+            <button className="buy-button"
               onClick={() => onBuyBook(book.id, book.ownerId)} // Pass book ID and owner ID
-              style={{
-                backgroundColor: '#28a745', // Green color
-                color: 'white',
-                border: 'none',
-                padding: '5px 10px',
-                borderRadius: '3px',
-                cursor: 'pointer',
-                marginRight: '10px',
-                fontSize: '0.8em',
-                transition: 'background-color 0.2s ease',
-              }}
             >
               Compra
             </button>
